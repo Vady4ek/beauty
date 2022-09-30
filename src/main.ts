fe68e75bd4bf42ceced5a0de3c83078469426ca0
Poscoi Vadim <poscoivadim@gmail.com>
2022-09-30 17:11:11 +0300

    Move routing to main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,59 @@ import { RouterModule } from '@angular/router';
 
 const routes = [
   {
+    path: '',
+    loadComponent: () =>
+      import('src/app/layouts/default/default.component').then(
+        (c) => c.DefaultComponent
+      ),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('src/app/pages/home/home.component').then(
+            (c) => c.HomeComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: 'order',
+    loadComponent: () =>
+      import('src/app/layouts/default/default.component').then(
+        (c) => c.DefaultComponent
+      ),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('src/app/pages/order/order.component').then(
+            (c) => c.OrderComponent
+          ),
+      },
+    ],
+  },
+  {
     path: 'auth',
     loadComponent: () =>
-      import('src/app/pages/auth/auth.component').then((c) => c.AuthComponent),
+      import('src/app/layouts/auth/auth.component').then(
+        (c) => c.AuthComponent
+      ),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('src/app/pages/auth/auth.component').then(
+            (c) => c.AuthComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('src/app/layouts/empty/empty.component').then(
+        (c) => c.EmptyComponent
+      ),
   },
 ];
 
